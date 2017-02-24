048296bcff5be3e238e93dd1715911ef9fd2f2e9
Matthew Sedam <sedammatthew@gmail.com>
2017-02-24 00:49:33 -0600

    Go open source See Issue #23 Update Readme.md

--- src/main/java/com/matthewsedam/cryptotrade/Electrum.java
+++ src/main/java/com/matthewsedam/cryptotrade/Electrum.java
@@ -1,9 +1,22 @@
-/*
+/* Electrum.java
+ * Description: Represents the Electrum Bitcoin wallet.
  * Created by Matthew Sedam on 2/6/2017.
+ *
  * Copyright 2017 Matthew Sedam.
- * Description: Represents the Electrum Bitcoin wallet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
+ 
 package com.matthewsedam.cryptotrade;
 
 import java.io.BufferedReader;
@@ -19,7 +32,7 @@ public class Electrum {
     private static final String ELECTRUM_BIN = "";
 
     /**
-     * 
+     *
      * @param pathAndArguments
      * @return
      * @throws Exception
@@ -45,7 +58,7 @@ public class Electrum {
     }
 
     /**
-     * 
+     *
      * @param pathAndArguments
      * @return
      * @throws Exception
@@ -56,7 +69,7 @@ public class Electrum {
     }
 
     /**
-     * 
+     *
      * @return
      * @throws Exception
      */
@@ -67,7 +80,7 @@ public class Electrum {
     }
 
     /**
-     * 
+     *
      * @param destinationAddress
      * @param amount
      * @throws Exception
