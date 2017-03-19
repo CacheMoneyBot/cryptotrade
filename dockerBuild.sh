d6a6bdd6506874f9b409f460dc33e9395892c137
Matthew Sedam <sedammatthew@gmail.com>
2017-03-19 02:01:02 -0400

    [WIP] Fixed dockerBuild.sh
     * Added exit status for script

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -2,12 +2,26 @@
 
 if [ "$1" == "run" ]
 then
-    docker run --name cryptotrade -v `pwd`:/cryptotrade chapel/chapel:latest \
-        /bin/sh -c "cd /cryptotrade; make; echo; build/cryptotrade; echo"
+    EXIT_STATUS=$(docker run --name cryptotrade -v `pwd`:/cryptotrade \
+    chapel/chapel:latest /bin/sh -c "cd /cryptotrade; make; echo; \
+    build/cryptotrade")
 else
-    docker run --name cryptotrade -v `pwd`:/cryptotrade chapel/chapel:latest \
-        /bin/sh -c "cd /cryptotrade; make $1; echo"
+    EXIT_STATUS=$(docker run --name cryptotrade -v `pwd`:/cryptotrade \
+    chapel/chapel:latest /bin/sh -c "cd /cryptotrade; make $1")
 fi
+
+echo
+echo "$EXIT_STATUS"
+echo
 
 docker stop cryptotrade
 docker rm cryptotrade
+
+if [[ "$EXIT_STATUS" == *"fail"* ]]
+then
+    EXIT_STATUS=1
+else
+    EXIT_STATUS=0
+fi
+
+exit $EXIT_STATUS
