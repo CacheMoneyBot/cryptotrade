b53273e3587b67008ddea4bb4e24b1cc48bf960f
Matthew Sedam <sedammatthew@gmail.com>
2017-05-14 00:29:31 -0400

    Made the test file grab all needed .chpl files (#51)
     * Excludes main.chpl
     * Excludes all test files except the current test

--- src/test/processTest.sh
+++ src/test/processTest.sh
@@ -5,8 +5,7 @@ find ../ -type d >inc.temp
 while IFS= read line; do flags="$flags -I$line"; done <inc.temp
 rm inc.temp
 
-file="../"${1#*/}
-file=${file//Test/}
+file=$(find ../ -name '*.chpl' ! -path '../main.chpl' ! -path '../test/*')
 
 chpl $file $1 -o ./test $flags
 ./test
